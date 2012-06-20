b63a4ed11891679321593df6eee59454af47ca81
Michele Riveiros Marino <rauggy@gmail.com>
2012-06-20 13:51:36 -0300

    Import inicial

---
+++ LyricsThingie/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TagLib.Id3v2;
+using System.Net;
+using System.Xml;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Configuration;
+
+namespace LyricsThingie {
+    class Program {
+        static void Main(string[] args) {
+            string directory = args[0];
+            //string directory = "E:\\Musicas";
+            StreamWriter write = null;
+
+            if (args.Length >= 2) {
+                string errors = args[1];
+                write = new StreamWriter(errors);
+                write.WriteLine("Arquivos cuja letra nao foi encontrada:");
+            }
+
+            bool forceDownload = Convert.ToBoolean(ConfigurationManager.AppSettings["ForceDownload"]);
+
+            Console.WriteLine("Iniciando processo, músicas que já possuem letra serão puladas.");
+
+            foreach (string fileName in Directory.GetFiles(directory, "*.mp3", SearchOption.AllDirectories)) {
+                TagLib.File file = TagLib.File.Create(fileName);
+
+                Console.Write("Procurando letra para {0} - {1}... ", file.Tag.FirstAlbumArtist, file.Tag.Title);
+
+                try {
+                    if (string.IsNullOrEmpty(file.Tag.Lyrics) || forceDownload) {
+                        file.Tag.Lyrics = GetLyricsForSong(file.Tag.FirstAlbumArtist, file.Tag.Title);
+                        file.Save();
+
+                        Console.Write("OK.\r\n");
+                    }
+                    else {
+                        Console.Write("SKIP.\r\n");
+                    }
+                }
+                catch (KeyNotFoundException) {
+                    if (write != null) {
+                        write.WriteLine("{0} | {1} - {2}", fileName, file.Tag.FirstAlbumArtist, file.Tag.Title);
+                        write.Flush();
+                    }
+                    Console.Write("Não foi encontrada letra.\r\n");
+                }
+                catch (ArgumentException) {
+                    if (write != null) {
+                        write.WriteLine("{0} | {1} - {2}", fileName, file.Tag.FirstAlbumArtist, file.Tag.Title);
+                        write.Flush();
+                    }
+                    Console.Write("Houve um problema ao obter a letra, tente novamente mais tarde.\r\n");
+                    
+                }
+                catch (WebException) {
+                    if (write != null) {
+                        write.WriteLine("{0} | {1} - {2}", fileName, file.Tag.FirstAlbumArtist, file.Tag.Title);
+                        write.Flush();
+                    }
+                    Console.Write("Houve um problema ao obter a letra, tente novamente mais tarde.\r\n");
+                }
+            }
+
+            if (write != null) {
+                write.Dispose();
+            }
+
+            Console.WriteLine("Processo finalizado, aperte enter para sair.");
+            Console.ReadLine();
+        }
+
+        static string GetLyricsForSong(string artist, string title) {
+            string letra = string.Empty;
+
+            letra = Providers.GetFromLyricWiki(artist, title);
+
+            if (string.IsNullOrEmpty(letra)) {
+                letra = Providers.GetFromTerra(artist, title);
+            }
+
+            if (string.IsNullOrEmpty(letra)) throw new KeyNotFoundException();
+
+            return letra;
+        }
+    }
+}
---
+++ LyricsThingie/Providers.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Xml;
+using HtmlAgilityPack;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace LyricsThingie {
+    class Providers {
+        private static Encoding ISOEncoding = Encoding.GetEncoding("ISO-8859-1");
+
+        public static string GetFromLyricWiki(string artist, string title) {
+            string rep = string.Empty;
+
+            artist = HttpUtility.UrlEncode((artist + ""), ISOEncoding).Replace(".", "%2E");
+            title = HttpUtility.UrlEncode((title + ""), ISOEncoding).Replace(".", "%2E");
+
+            //Utilizar o serviço PUSH para verificar se obtemos a música.
+            WebRequest request = HttpWebRequest.Create(string.Format("http://lyrics.wikia.com/api.php?func=getSong&artist={0}&song={1}&fmt=xml", artist, title));
+            WebResponse response = request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            XmlDocument doc1 = new XmlDocument();
+            string req = reader.ReadToEnd();
+            doc1.LoadXml(req);
+
+            //Verificar se eles tem a letra
+            XmlNode firstNode = doc1.SelectSingleNode("//lyrics");
+            if (firstNode == null) return string.Empty;
+            if (doc1.SelectSingleNode("//lyrics").InnerText == "Not found") return string.Empty;
+
+            //Caso tenham, fazer uma requisição a URL onde está a letra completa
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(doc1.SelectSingleNode("//url").InnerText);
+            HtmlNode node = doc.DocumentNode.SelectSingleNode("//div[@class='lyricbox']");
+
+            if (node == null) return string.Empty;
+
+            //Remover elementos desnecessários
+            node.FirstChild.Remove();
+            node.LastChild.Remove();
+            node.LastChild.Remove();
+            node.LastChild.Remove();
+            node.LastChild.Remove();
+
+            //Trocar os <br> por quebras de linha convencionais
+            rep = Regex.Replace(WebUtility.HtmlDecode(node.InnerHtml), "\\s*<br ?\\/?>\\s*", "\r\n");
+            //Remover qualquer tag html presente
+            rep = Regex.Replace(rep, "<[^>]+>", "");
+
+            return rep;
+        }
+
+        public static string GetFromTerra(string artist, string title) {
+            string rep = string.Empty;
+
+            artist = (artist + "").ToLowerInvariant();
+            title = (title + "").ToLowerInvariant();
+
+            //Obter a letra da música
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(string.Format("http://letras.mus.br/winamp.php?t={0}-{1}", HttpUtility.UrlEncode(artist, ISOEncoding), HttpUtility.UrlEncode(title, ISOEncoding)));
+            HtmlNode node = doc.DocumentNode.SelectSingleNode("//div[@id='letra']/p");
+
+            //Se encontrar a letra, retorna
+            if (node != null) {
+                rep = WebUtility.HtmlDecode(node.InnerText);
+                return rep;
+            }
+
+            //Se não encontrar, vamos trocar os "&" por "e" e ver se encontramos
+            if (artist.Contains("&") || title.Contains("&")) {
+                artist = artist.Replace('&', 'e');
+                title = title.Replace('&', 'e');
+
+                doc = web.Load(string.Format("http://letras.mus.br/winamp.php?t={0}-{1}", HttpUtility.UrlEncode(artist, ISOEncoding), HttpUtility.UrlEncode(artist, ISOEncoding)));
+                node = doc.DocumentNode.SelectSingleNode("//div[@id='letra']/p");
+
+                if (node != null) {
+                    rep = WebUtility.HtmlDecode(node.InnerText);
+                }
+            }
+
+            return rep;
+        }
+
+    }
+}
